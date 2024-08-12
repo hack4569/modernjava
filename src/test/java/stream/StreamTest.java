c222db10c0f2e6de12650b205d5d80d486a98cd6
lsh <hack4569@naver.com>
2024-08-13 05:58:29 +0900

    스트림 분할, 그룹핑 study

--- src/test/java/stream/StreamTest.java
+++ src/test/java/stream/StreamTest.java
@@ -5,13 +5,13 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static java.util.Comparator.comparing;
-import static java.util.stream.Collectors.toList;
+import static java.util.Comparator.comparingInt;
+import static java.util.stream.Collectors.*;
 
 public class StreamTest {
     @Test
@@ -25,9 +25,9 @@ public class StreamTest {
                 new Dish("pork", false, 800, Dish.Type.MEAT),
                 new Dish("beef", false, 300, Dish.Type.MEAT),
                 new Dish("chicken", false, 500, Dish.Type.MEAT),
-                new Dish("rice", true, 660, Dish.Type.OTHER),
+                new Dish("rice", true, 160, Dish.Type.OTHER),
                 new Dish("salmon", true, 300, Dish.Type.FISH),
-                new Dish("prawns", false, 100, Dish.Type.FISH)
+                new Dish("prawns", false, 600, Dish.Type.FISH)
         );
     }
 
@@ -87,4 +87,99 @@ public class StreamTest {
                 .count();
         System.out.println(cnt);
     }
+    @Test
+    void OptionalInt() {
+        OptionalInt maxCalories = menu.stream().mapToInt(Dish::getCalories).max();
+        int max = maxCalories.orElse(1);
+        System.out.println(max);
+    }
+
+    @Test
+    void 스트림만들기() {
+        //값으로 스트림 만들기
+        Stream<String> stream = Stream.of("Modern", "Java","In");
+        stream.map(String::toUpperCase).forEach(System.out::println);
+
+        //null이 될 수 있는 객체로 스트림 만들기
+        String homeValue = System.getProperty("home");
+        Stream<String> homeValueStream = homeValue == null ? Stream.empty() : Stream.of(homeValue);
+        Stream<String> homeValueStream1 = Stream.ofNullable(System.getProperty("home"));
+
+        //배열을 스트림으로 만들기
+        int[] numbers = {1,2,3,4};
+        int sum = Arrays.stream(numbers).sum();
+
+        //함수로 스트림 만들기
+        Stream.iterate(0, n -> n+2).limit(10).forEach(System.out::println);
+    }
+
+    //요약 연산
+    @Test
+    void collectTest() {
+        int sum = menu.stream().collect(summingInt(Dish::getCalories));
+        System.out.println(sum);
+        int sum2 = menu.stream().collect(reducing(0, Dish::getCalories, (i, j) ->i + j));
+        System.out.println(sum2);
+
+        IntSummaryStatistics summary = menu.stream().collect(summarizingInt(Dish::getCalories));
+        System.out.println(summary);
+
+        String menuName = menu.stream().map(Dish::getName).collect(Collectors.joining(", "));
+        System.out.println(menuName);
+    }
+
+    public enum CaloricLevel {DIET, NORMAL, FAT};
+    //그룹화
+    @Test
+    void groupTest() {
+        Map<Dish.Type, List<Dish>> groupMap = menu.stream().collect(groupingBy(Dish::getType));
+        System.out.println(groupMap);
+
+        Map<Dish.Type, List<Dish>> groupMap1 = menu.stream().filter(i -> i.getCalories() < 300).collect(groupingBy(Dish::getType));
+        System.out.println(groupMap1);
+
+        Map<Dish.Type, List<Dish>> groupMap2 = menu.stream().collect(groupingBy(Dish::getType, filtering(i -> i.getCalories() < 300, toList())));
+        System.out.println(groupMap2);
+
+        Map<Dish.Type, Map<CaloricLevel, List<Dish>>> groupedDishes = menu.stream().collect(
+                groupingBy(Dish::getType, groupingBy(dish -> {
+                    if (dish.getCalories() < 400)
+                        return CaloricLevel.DIET;
+                    else if (dish.getCalories() < 700)
+                        return CaloricLevel.NORMAL;
+                    else return CaloricLevel.FAT;
+                }))
+        );
+        System.out.println(groupedDishes);
+
+        //각 타입별 최대값
+        Map<Dish.Type, Dish> mostCaloricByType =
+                menu.stream()
+                        .collect(
+                                groupingBy(Dish::getType, collectingAndThen(
+                                        maxBy(comparingInt(Dish::getCalories)),
+                                        Optional::get
+                                ))
+                        );
+        System.out.println(mostCaloricByType);
+
+    }
+
+    @Test
+    void 분할_테스트() {
+        Map<Boolean, Dish> mostCaloricPartitionedByVegetarian =
+                menu.stream().collect(
+                        partitioningBy(Dish::isVegetarian,
+                                collectingAndThen(
+                                        maxBy(comparingInt(Dish::getCalories)),
+                                        Optional::get
+                                )
+                        )
+                );
+        System.out.println(mostCaloricPartitionedByVegetarian);
+    }
 }
+
+
+
+
